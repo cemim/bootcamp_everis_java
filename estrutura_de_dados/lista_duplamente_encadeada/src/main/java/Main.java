314286c0b68df6500480614a829d63c3e4b59a9a
Filipe Cemim <filipe.cemim@gmail.com>
2021-09-08 22:37:48 -0300

    Aula listas duplamente encadeadas

--- estrutura_de_dados/lista_duplamente_encadeada/src/main/java/Main.java
+++ estrutura_de_dados/lista_duplamente_encadeada/src/main/java/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+        ListaDuplamenteEncadeada<String> minhaListaEncadeada = new ListaDuplamenteEncadeada<>();
 
+        minhaListaEncadeada.add("C1");
+        minhaListaEncadeada.add("C2");
+        minhaListaEncadeada.add("C3");
+        minhaListaEncadeada.add("C4");
+        minhaListaEncadeada.add("C5");
+        minhaListaEncadeada.add("C6");
+        minhaListaEncadeada.add("C7");
+
+        System.out.println(minhaListaEncadeada);
+
+        minhaListaEncadeada.remove(3);
+
+        System.out.println(minhaListaEncadeada);
+
+        minhaListaEncadeada.add(3, "99");
+
+        System.out.println(minhaListaEncadeada);
     }
 }
