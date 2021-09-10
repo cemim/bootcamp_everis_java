27e7433c079599d7494c6ed20471a7bd3fb503c3
Filipe Cemim <filipe.cemim@gmail.com>
2021-09-10 01:42:23 -0300

    Aula lista circular

--- estrutura_de_dados/listas_circulares/src/main/java/Main.java
+++ estrutura_de_dados/listas_circulares/src/main/java/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        ListaCircular<String> listaCircular = new ListaCircular<>();
 
+        listaCircular.add("C1");
+        listaCircular.add("C2");
+        listaCircular.add("C3");
+        listaCircular.add("C4");
+        listaCircular.add("C5");
+        listaCircular.add("C6");
+
+//        System.out.println(listaCircular);
+//        listaCircular.remove(3);
+//        System.out.println(listaCircular);
+
+        for (int i = 0; i < 12; i++){
+            System.out.println(listaCircular.get(i));
+        }
     }
 }
